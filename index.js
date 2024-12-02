56144e58fb2c6f5b424e5627b4951ef3a96618ad
umutkurt95 <umutgurt@gmail.com>
2024-12-02 11:32:17 +0100

    İlk commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// const emojis = ['🐥','🐯','🐼']
+// console.log(emojis.includes('🐴'))
+
+const addItemBtn = document.getElementById('add-item-btn')
+const itemInput = document.getElementById('item-input')
+const list = document.getElementById('list')
+
+const shoppingList = []
+    
+addItemBtn.addEventListener('click', function(){
+
+if (shoppingList.includes(itemInput.value)){
+    itemInput.value = ''
+} else {
+    shoppingList.push(itemInput.value)
+    render() 
+}
+
+itemInput.value = ''        
+})
+
+function render(){    
+    let html = ''
+    for (let item of shoppingList){
+        html+= `<li class="list-item">${item}</li>`
+    }
+    list.innerHTML = html
+}
+
+render()
+
